a42848d5aa9446ff4671a1e763be2e5b7edacfad
bmacaw <mzmacaw@gmail.com>
2017-03-14 16:40:45 -0600

    Edit reformat code

--- app/src/main/java/com/example/android/products/EditorActivity.java
+++ app/src/main/java/com/example/android/products/EditorActivity.java
@@ -35,7 +35,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.android.products.data.ProductContract.ProductEntry;
